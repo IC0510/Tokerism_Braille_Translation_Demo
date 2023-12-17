7a5955e522d97c439ee8dfcaa95455e915d24122
Ian <developer@krystal.institute>
2023-12-17 18:48:40 +0800

    Initial Commit

---
+++ Prototype_For_Tokerism/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype_For_Tokerism
+//
+//  Created by Ian CHAN on 17/12/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
